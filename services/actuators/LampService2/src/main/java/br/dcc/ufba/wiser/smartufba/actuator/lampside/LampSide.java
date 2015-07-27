b7cb46a740844b71a29289461f354923062d2fde
jefersonla <oi_je@hotmail.com>
2015-07-26 21:44:16 -0300

    Adicionado servili lampada 2

---
+++ services/actuators/LampService2/src/main/java/br/dcc/ufba/wiser/smartufba/actuator/lampside/LampSide.java
@@ -0,0 +1,24 @@
+/*
+ * Created by Wiser Research Group UFBA
+ */
+package br.dcc.ufba.wiser.smartufba.actuator.lampside;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Lamp")
+public class LampSide {
+    private boolean status; 
+
+    public LampSide(){
+        status = false;
+    }
+    
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+}
